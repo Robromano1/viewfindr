111e4920091e8adde330126389aeb24ee61cee5a
Robert <Robromano2@gmail.com>
2021-05-31 10:43:53 -0400

    Create firestore hook and image grid component to retrieve images from firestore database, and display them in a grid format.

--- viewfindr/src/components/ImageGrid.js
+++ viewfindr/src/components/ImageGrid.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import useFirestore from '../hooks/useFirestore';
 
 export const ImageGrid = () => {
+	const { docs } = useFirestore('images');
+	
 	return (
 		<div className="img-grid">
-			images
+			{ docs && docs.map(doc => (
+				<div className="img-wrap" key={doc.id}>
+					<img src={doc.url} alt="new photo" />
+				</div>
+			))}
 		</div>
 	)
 }
