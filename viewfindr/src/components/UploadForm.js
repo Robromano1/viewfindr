e005dd3185d6555125eb9cb25257a40bfe673d07
Robert <Robromano2@gmail.com>
2021-05-30 23:51:22 -0400

    Create and set up upload form component to upload images

--- viewfindr/src/components/UploadForm.js
+++ viewfindr/src/components/UploadForm.js
@@ -2,19 +2,29 @@ import React, { useState } from "react";
 
 export const UploadForm = () => {
   const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+
+  const types = ["image/png", "image/jpeg"];
 
   const handleChange = (e) => {
     let selected = e.target.files[0];
-    // console.log(selected);
 
-		if (selected) {
-			setFile(selected);
-		}
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+			setError('');
+    } else {
+      setFile(null);
+      setError("Please select and image file (png or jpeg)");
+    }
   };
 
   return (
     <form>
       <input type="file" onChange={handleChange} />
+			<div className="output">
+				{ error && <div className="error">{error}</div> }
+				{ file && <div>{ file.name }</div>}
+			</div>
     </form>
   );
 };
